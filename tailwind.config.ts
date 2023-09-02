3028affb4bf13b10e085033fd31061962844762e
ApMatheus <matheusaparecido101@gmail.com>
2023-09-02 01:20:22 -0300

    feat(CategoryList) create category list

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,5 +16,14 @@ export default {
         "pre-header": "#000000",
       },
     },
+    fontSize: {
+      sm: "0.94rem",
+      base: "1rem",
+      xl: ".940rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
   },
 };
