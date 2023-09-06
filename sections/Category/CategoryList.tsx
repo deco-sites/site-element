af36af855ab151d68a61808c457447e3d9b0f4f6
ApMatheus <matheusaparecido101@gmail.com>
2023-09-06 19:51:39 -0300

    fix(CategoryList) scale in imgs

--- sections/Category/CategoryList.tsx
+++ sections/Category/CategoryList.tsx
@@ -103,11 +103,11 @@ function CategoryList(props: Props) {
         ) => (
           <Slider.Item
             index={index}
-            class="flex flex-col gap-4 carousel-item first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0 sm:w-1/5 w-11/12 lg:w-full lg:max-w-[348px]"
+            class="flex flex-col gap-4 carousel-item first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0 sm:w-1/5 w-11/12 lg:w-full lg:max-w-[285px] 2xl:max-w-[348px]"
           >
             <a
               href={href}
-              class="flex flex-col lg:max-w-[348px] sm:max-w-[11rem] w-[calc(100vw - 70px)] lg:h-auto"
+              class="flex flex-col lg:max-w-[285px] 2xl:max-w-[348px] sm:max-w-[11rem] w-[calc(100vw - 70px)] lg:h-auto"
             >
               {layout.categoryCard?.textPosition === "top" &&
                 (
@@ -120,9 +120,9 @@ function CategoryList(props: Props) {
                 )}
               {image &&
                 (
-                  <figure>
+                  <figure class={" overflow-hidden"}>
                     <Image
-                      class="w-full"
+                      class="w-full hover:scale-110 ease-in-out transform duration-200 "
                       src={image}
                       alt={description || label || tag}
                       width={290}
